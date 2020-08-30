d733d83593988062c352ddf2129db877f7a1a4ab
yahayakenny <y.hkehinde@yahoo.com>
2020-08-30 10:08:19 +0100

    added style

---
+++ src/components/Answers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Answers extends Component {
+    render() {
+        return (
+            <div>
+                Answers
+            </div>
+        )
+    }
+}
+
+export default Answers
